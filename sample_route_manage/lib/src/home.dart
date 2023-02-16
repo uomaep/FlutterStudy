7dbc619f03fa424316de161015ab4541fa7d1146
uomaep <uomaep@gmail.com>
2023-02-16 19:47:29 +0900

    initial commit

---
+++ sample_route_manage/lib/src/home.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("라우트 관리 홈"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text("일반적인 라우트"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
