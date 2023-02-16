fcf7d6c402a955a59914b116a0a31f355e30f103
uomaep <uomaep@gmail.com>
2023-02-16 18:55:32 +0900

    feat: bottom navigation bar
    bottom navigation bar coding ...
    아직 클릭 시 기능은 없다.

--- instagram/lib/src/app.dart
+++ instagram/lib/src/app.dart
@@ -1,10 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:instagram/src/components/image_data.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return WillPopScope(
+      child: Scaffold(
+        appBar: AppBar(),
+        body: Container(),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          currentIndex: 0,
+          elevation: 0,
+          onTap: (value) {},
+          items: [
+            BottomNavigationBarItem(
+              icon: ImageData(IconPath.homeOff),
+              activeIcon: ImageData(IconPath.homeOn),
+              label: 'home',
+            ),
+            BottomNavigationBarItem(
+              icon: ImageData(IconPath.searchOff),
+              activeIcon: ImageData(IconPath.searchOn),
+              label: 'home',
+            ),
+            BottomNavigationBarItem(
+              icon: ImageData(IconPath.uploadIcon),
+              label: 'home',
+            ),
+            BottomNavigationBarItem(
+              icon: ImageData(IconPath.activeOff),
+              activeIcon: ImageData(IconPath.activeOn),
+              label: 'home',
+            ),
+            BottomNavigationBarItem(
+              icon: Container(
+                width: 30,
+                height: 30,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.grey,
+                ),
+              ),
+              label: 'home',
+            ),
+          ],
+        ),
+      ),
+      onWillPop: () async {
+        return false;
+      },
+    );
   }
 }
